68c11bc708b81f1aadf474ef5c81b3e8d5c41890
Gigena Developer <nahuel.gigena@hotmail.com>
2022-11-05 23:39:37 -0300

    Add function get  all users

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,25 +1,29 @@
-const getAllUsers=()=>{
-    return;
-}
-const getOneUser=()=>{
-    return;
-}
-const updateOneUser=()=>{
-    return;
-}
-const createNewUser=()=>{
-    return;
-}
-const deleteOneUser=()=>{
-    return;
-}
+import mongoose from "mongoose";
+import User from "../models/UserModel.js";
 
+const getAllUsers = async () => {
+  const allUsers = await User.find(); 
+  mongoose.connection.close();
+  
+  return allUsers;
+};
+const getOneUser = async () => {
+  return allUsers;
+};
+const updateOneUser = () => {
+  return;
+};
+const createNewUser = () => {
+  return;
+};
+const deleteOneUser = () => {
+  return;
+};
 
 export default {
-    getAllUsers,
-    getOneUser,
-    updateOneUser,
-    createNewUser,
-    deleteOneUser,
+  getAllUsers,
+  getOneUser,
+  updateOneUser,
+  createNewUser,
+  deleteOneUser,
 };
-  
