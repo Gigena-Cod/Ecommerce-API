e8c1bf67ce4a4d7ed491b4c914ede325bf049ed1
Gigena Developer <nahuel.gigena@hotmail.com>
2022-11-20 15:47:08 -0300

    Fix connection Database

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 
-dotenv.config()
-const conectiongString = process.env.MONGO_DB_URI
+dotenv.config();
+const conectiongString = process.env.MONGO_DB_URI;
 
 const initialConnect = async () => {
-  mongoose.connect(conectiongString,)
-  .then(() =>   console.log('Database connected')       
-  )
-  .catch(() =>  console.log('Database Problem with connected')) 
-  
+  mongoose
+    .connect(conectiongString)
+    .then(() => console.log("Database connected"))
+    .catch(() => console.log("Database Problem with connected"));
 };
 
-export default  initialConnect
-
-
+export default initialConnect;
