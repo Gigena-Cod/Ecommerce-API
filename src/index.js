1b128d85db00e9681588bbf575fa77b5bc87e842
Gigena Developer <nahuel.gigena@hotmail.com>
2022-11-20 16:46:01 -0300

    Fix Cors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan"; 
-import initialConnect from './database/mongo.js'
+import morgan from "morgan";
+import initialConnect from "./database/mongo.js";
 import middlewares from "./middlewares/index.js";
 import v1UserRouter from "./v1/routes/userRoutes.js";
 import v1AuthRouter from "./v1/routes/authRoutes.js";
 
 const corsOpts = {
-  origin: '*',
-
-  methods: [
-    'GET',
-    'POST',
-  ],
-
-  allowedHeaders: [
-    'Content-Type',
-  ],
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "token"],
 };
 
-initialConnect()
+initialConnect();
 
 const app = express();
-app.use(morgan('tiny'))
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cors(corsOpts));
 const PORT = process.env.PORT;
 
 app.use("/api/v1/users", v1UserRouter);
 app.use("/api/v1/sesion", v1AuthRouter);
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on Port ${PORT}`);
