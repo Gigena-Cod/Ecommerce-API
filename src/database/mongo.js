9223db2ec2739c80e986c853c876e010403b6eb8
Gigena Developer <nahuel.gigena@hotmail.com>
2022-11-20 15:53:12 -0300

    Fix file .env

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 const conectiongString = process.env.MONGO_DB_URI;
+console.log("🚀 ~ file: mongo.js ~ line 6 ~ conectiongString", conectiongString)
 
 const initialConnect = async () => {
   mongoose
